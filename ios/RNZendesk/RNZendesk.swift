d257271470e75d1baf8464eb9c7cacbb5590912f
Trent Ewald <trent@caribu.com>
2019-01-04 23:01:40 -0800

    Open help center.

--- ios/RNZendesk/RNZendesk.swift
+++ ios/RNZendesk/RNZendesk.swift
@@ -17,18 +17,18 @@ class RNZendesk: RCTEventEmitter {
     override open static func requiresMainQueueSetup() -> Bool {
         return false;
     }
-    
+
     @objc(constantsToExport)
     override func constantsToExport() -> [AnyHashable: Any] {
         return [:]
     }
-    
+
     @objc(supportedEvents)
     override func supportedEvents() -> [String] {
         return []
     }
-    
-    
+
+
     // MARK: - Public API
 
     @objc(initialize:)
@@ -37,11 +37,13 @@ class RNZendesk: RCTEventEmitter {
             let appId = config["appId"] as? String,
             let clientId = config["clientId"] as? String,
             let zendeskUrl = config["zendeskUrl"] as? String else { return }
-        
+
         Zendesk.initialize(appId: appId, clientId: clientId, zendeskUrl: zendeskUrl)
         Support.initialize(withZendesk: Zendesk.instance)
+        let identity = Identity.createAnonymous()
+        Zendesk.instance?.setIdentity(identity)
     }
-    
+
     @objc(identifyJWT:)
     func identifyJWT(token: String?) {
         guard let token = token else { return }
@@ -52,22 +54,22 @@ class RNZendesk: RCTEventEmitter {
     @objc(identifyAnonymous)
     func identifyAnonymous() {
         let identity = Identity.createAnonymous()
-        
+
         Zendesk.instance?.setIdentity(identity)
     }
-    
+
     @objc(showHelpCenter:)
     func showHelpCenter(with options: [String: Any]) {
         DispatchQueue.main.async {
-            let hcConfig = HelpCenterUiConfiguration()
-            hcConfig.hideContactSupport = (options["hideContactSupport"] as? Bool) ?? false
-            let helpCenter = HelpCenterUi.buildHelpCenterOverview(withConfigs: [hcConfig])
-            
+
+            let helpCenter = HelpCenterUi.buildHelpCenterOverviewUi(withConfigs: [])
+
+
             let nvc = UINavigationController(rootViewController: helpCenter)
             UIApplication.shared.keyWindow?.rootViewController?.present(nvc, animated: true, completion: nil)
         }
     }
-    
+
     @objc(showNewTicket:)
     func showNewTicket(with options: [String: Any]) {
         DispatchQueue.main.async {
@@ -76,7 +78,7 @@ class RNZendesk: RCTEventEmitter {
                 config.tags = tags
             }
             let requestScreen = RequestUi.buildRequestUi(with: [config])
-            
+
             let nvc = UINavigationController(rootViewController: requestScreen)
             UIApplication.shared.keyWindow?.rootViewController?.present(nvc, animated: true, completion: nil)
         }
