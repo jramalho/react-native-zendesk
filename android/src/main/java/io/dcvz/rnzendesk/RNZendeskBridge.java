3f3becbec1c0c5c6d894807799743bcb08a8bcb9
Trent Ewald <trent@caribu.com>
2019-01-04 23:05:52 -0800

    Android Help Center

--- android/src/main/java/io/dcvz/rnzendesk/RNZendeskBridge.java
+++ android/src/main/java/io/dcvz/rnzendesk/RNZendeskBridge.java
@@ -1,13 +1,13 @@
 package io.dcvz.rnzendesk;
 
 import zendesk.core.Zendesk;
+import zendesk.core.AnonymousIdentity;
 import zendesk.core.Identity;
 import zendesk.core.JwtIdentity;
 import zendesk.support.Support;
 import zendesk.support.UiConfig;
 import zendesk.support.guide.HelpCenterActivity;
 import zendesk.support.request.RequestActivity;
-
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -31,8 +31,12 @@ public class RNZendeskBridge extends ReactContextBaseJavaModule {
         String appId = config.getString("appId");
         String zendeskUrl = config.getString("zendeskUrl");
         String clientId = config.getString("clientId");
+        Zendesk.INSTANCE.init(getReactApplicationContext(), zendeskUrl,
+    appId,
+    clientId);
+        Identity identity = new AnonymousIdentity();
+        Zendesk.INSTANCE.setIdentity(identity);
 
-        Zendesk.INSTANCE.init(getReactApplicationContext(), zendeskUrl, appId, clientId);
         Support.INSTANCE.init(Zendesk.INSTANCE);
     }
 
@@ -44,14 +48,19 @@ public class RNZendeskBridge extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void showHelpCenter(ReadableMap options) {
+      /*
         UiConfig hcConfig = HelpCenterActivity.builder()
                 .withContactUsButtonVisible(!(options.hasKey("hideContactSupport") && options.getBoolean("hideContactSupport")))
                 .config();
 
         HelpCenterActivity.builder()
                 .show(getReactApplicationContext(), hcConfig);
+*/
+
+             HelpCenterActivity.builder()
+    .show(getReactApplicationContext());
     }
-    
+
     @ReactMethod
     public void showNewTicket(ReadableMap options) {
         ArrayList tags = options.getArray("tags").toArrayList();
