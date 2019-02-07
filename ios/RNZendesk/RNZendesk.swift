aab3de37431a86ad62a97e2f03f4dbb0402f9dbe
Trent Ewald <trent@caribu.com>
2019-02-07 12:27:17 -0800

    ZenDesk iOS custom fields

--- ios/RNZendesk/RNZendesk.swift
+++ ios/RNZendesk/RNZendesk.swift
@@ -60,9 +60,32 @@ class RNZendesk: RCTEventEmitter {
 
     @objc(showHelpCenter:)
     func showHelpCenter(with options: [String: Any]) {
-        DispatchQueue.main.async {
 
-            let helpCenter = HelpCenterUi.buildHelpCenterOverviewUi(withConfigs: [])
+        DispatchQueue.main.async {
+            let Device_Brand = options["Device_Brand"] as? String;
+            let Device_Model = options["Device_Model"] as? String;
+            let OS = options["OS"] as? String;
+            let OS_Version = options["OS_Version"] as? String;
+            let App_Version = options["App_Version"] as? String;
+            let Connection = options["Connection"] as? String;
+            let Phone_or_Tablet = options["Phone_or_Tablet"] as? String;
+
+
+            let c2 = ZDKCustomField(fieldId: NSNumber(value: 360016501732), andValue: Device_Brand!)
+            let c3 = ZDKCustomField(fieldId: NSNumber(value: 360016554711), andValue: Device_Model!)
+            let c4 = ZDKCustomField(fieldId: NSNumber(value: 360016554731), andValue: OS!)
+            let c5 = ZDKCustomField(fieldId: NSNumber(value: 360016554911), andValue: OS_Version!)
+            let c6 = ZDKCustomField(fieldId: NSNumber(value: 360016554751), andValue: App_Version!)
+            let c7 = ZDKCustomField(fieldId: NSNumber(value: 360016502792), andValue: Connection!)
+            let c8 = ZDKCustomField(fieldId: NSNumber(value: 360016556691), andValue: Phone_or_Tablet!)
+
+            /*
+            config.subject = "iOS Ticket"
+            config.tags = ["ios", "mobile"]
+            */
+            let config = RequestUiConfiguration()
+            config.fields = [c2,c3,c4,c5,c6,c7,c8] as! [ZDKCustomField]
+            let helpCenter = HelpCenterUi.buildHelpCenterOverviewUi(withConfigs: [config])
 
 
             let nvc = UINavigationController(rootViewController: helpCenter)
